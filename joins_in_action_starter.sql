9624b09a62d0bbbdd0c72d3d8697dfdd80f02b35
Jeanking23 <jeandedieu405@gmail.com>
2023-03-14 15:26:00 -0400

    updating qerury

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -10,31 +10,44 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a left join to see all records from the movies table and matching records from the users table.
--- EXPECTED RESULT: 75 Rows Returned
+-- EXPECTED RESULT: 75 Rows Returned,
+SELECT * FROM movies LEFT JOIN users on movies.movie_id = favorite_movie_id;
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
-
+SELECT * FROM users RIGHT JOIN movies on users.user_id = movies.movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
-
+SELECT * FROM movies INNER JOIN users ON movies.movie_id = users.favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
-
+SELECT movies.lead_studio, SUM(users.favorite_movie_id) AS total_favorites
+FROM movies 
+INNER JOIN users
+ON movies.movie_id = users.favorite_movie_id
+GROUP By movies.lead_studio
+ORDER BY total_favorites DESC
+LIMIT 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
 -- EXPECTED RESULT: 52.21
-
+SELECT 	AVG(movies.rotten_tomatoes) AS avg_score
+FROM movies 
+INNER JOIN users 
+ON movies.movie_id = users.favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
--- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
+-- filmDetermine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
-
+SELECT film, COUNT(*) AS count
+FROM movies
+GROUP BY film
+HAVING count > 1
